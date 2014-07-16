3cb7a3e76d84e5732746a0fba39063682f256c34
nugtrio <jason.an18@gmail.com>
2014-07-15 23:38:11 -0400

    Explicit import of pyodbc

--- db_interface.py
+++ db_interface.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker, joinedload, subqueryload
 from db_model import Campaign, Contribution, Comment, Person, Venture
 import datetime
+import pyodbc
 
 engine=create_engine('mssql+pyodbc://pilotfish:setinstone@pilotfishdb.c5zdfsvfmy5u.us-west-2.rds.amazonaws.com:1433/FishBase', echo=True)
 engine.connect()
