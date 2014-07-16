f83978e0e7bcb8c375c3d164d01bf5195a3fca83
nugtrio <jason.an18@gmail.com>
2014-07-15 21:45:06 -0400

    Reverting back to Flask(__name__)

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import flash, url_for, render_template, redirect, request, g, session
+from flask import Flask, flash, url_for, render_template, redirect, request, g, session
 from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
 from db_interface import  all_campaigns, get_campaign_by_title, get_all_persons, get_person_by_id, get_contribution, get_ventures, commit_to_db
 from db_model import Campaign, Contribution, Comment, Person
 from forms import SignUpForm, LogInForm, PostForm
 import datetime
 
-application = flask.Flask(__name__)
+application = Flask(__name__)
 application.config.from_object('config')
 lm = LoginManager()
 lm.init_app(application)
